cb108abfdfcffbc09b0aa34f2880753f904a64e1
Felipe Toro <ftorocastilla@gmail.com>
2025-01-30 16:53:46 -0300

    feat: exercise 3.11, adjust statics files to deploy backend and frontend app

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ let persons = [
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :response-time ms :body'))
